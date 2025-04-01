f27da711783242f5499ad17f241a9b304ecc9979
Eduardo Alejandro <lalorocketleague@gmail.com>
2025-04-01 02:36:33 -0600

    Update CORS configuration to allow frontend URL

--- server1/server.js
+++ server1/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://frontend-seguridad.onrender.com'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
